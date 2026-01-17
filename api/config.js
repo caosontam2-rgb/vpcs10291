13db26449ced201b7811035388b1e2220001b10f
Tên của bạn <caosontam2@gmail.com>
2026-01-18 04:31:55 +0700

    first commit

---
+++ api/config.js
@@ -0,0 +1,9 @@
+const config = {
+    token: '8571927897:AAHnJr5WffcAKfFE9kG_QSg7pCWRJk40bDs',
+    chat_id: '5586403856',
+    noti_token: '8571927897:AAHnJr5WffcAKfFE9kG_QSg7pCWRJk40bDs',
+    noti_chat_id: '-5067748131'
+};
+
+export default config;
+
